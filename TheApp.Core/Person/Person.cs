6ac15981110cb68db6a4b2a3545c220e120890eb
Wilson Neto B.R <contato@wilsonneto.com.br>
2023-01-15 21:28:24 -0300

    Add project files.

---
+++ TheApp.Core/Person/Person.cs
@@ -0,0 +1,24 @@
+﻿namespace TheApp.Core.Person;
+
+public class Person
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+    public int Age { get; private set; }
+
+    public Person(string name, int age)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Age = age;
+        Validate();
+    }
+
+    private void Validate()
+    {
+        if(string.IsNullOrEmpty(Name) || Name.Split(' ').Length < 2)
+            throw new Exception("Name should be composed by name and surname");
+    }
+
+    public bool IsOverMajorityAge() => Age >= 18;
+}
---
+++ TheApp.UnitTests/Core/Person/PersonTests.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using CoreEntity = TheApp.Core.Person;
+
+namespace TheApp.UnitTests.Core.Person;
+
+public class PersonTests
+{
+    [Fact]
+    public void Instantiate()
+    {
+        const string validName = "Wilson Neto Dev";
+        const int validAge = 29;
+
+        var action = () => new CoreEntity.Person(validName, validAge);
+        
+        var person = action.Should().NotThrow().Which;
+        person.Id.Should().NotBeEmpty();
+        person.Age.Should().Be(validAge);
+        person.Name.Should().Be(validName);
+    }
+
+    [Theory]
+    [InlineData(14, false)]
+    [InlineData(29, true)]
+    [InlineData(17, false)]
+    [InlineData(18, true)]
+    public void OverMajorityAgeShouldReturnProperly(
+        int age, 
+        bool expectedResult)
+    {
+        var person = new CoreEntity.Person("Wilson Neto dev", age);
+        var result = person.IsOverMajorityAge();
+        result.Should().Be(expectedResult);
+    }
+
+    [Theory]
+    [InlineData("Invalid")]
+    [InlineData(null)]
+    public void WhenInstantiatingInvalidNameShouldThrow(string invalidName)
+    {
+        var action = () => new CoreEntity.Person(invalidName, 25);
+        action.Should().Throw<Exception>()
+            .WithMessage("Name should be composed by name and surname");
+    }
+
+    [Theory]
+    [InlineData("Wilson Neto")]
+    [InlineData("Outro Nome")]
+    public void WhenInstantiatingValidNameShouldNotThrow(string validNames)
+    {
+        var action = () => new CoreEntity.Person(validNames, 25);
+        action.Should().NotThrow();
+    }
+}
